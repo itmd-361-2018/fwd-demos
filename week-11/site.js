6975f3ec58514ca62075eb4de6915c07d8a12ae0
Karl Stolley <karl.stolley@gmail.com>
2018-10-29 16:07:48 -0500

    Add JavaScript to listen for the loaded DOM and clicks on #heading

--- week-11/site.js
+++ week-11/site.js
@@ -11,4 +11,26 @@
   // Now we can get down to business and write modern
   // unobtrusive JavaScript...
 
+  document.addEventListener('DOMContentLoaded', function() {
+    // Pretty much all of our page's JavaScript that works
+    // on the DOM will be written in here...
+
+    // Add a js class on the <html> element...
+    document.querySelector('html').className = 'js';
+
+    // Select and replace the contents of #heading
+    var heading = document.querySelector('#header');
+    heading.innerHTML = "<a href='#content'>Click to Show Content</a>";
+
+    // Show content...
+
+    // Listen for click events on the heading...
+    heading.addEventListener('click', function() {
+      // When it's clicked, add a 'visible' class to our
+      // #content element
+      document.querySelector('#content').className = 'visible';
+    });
+  });
+
+
 })();
