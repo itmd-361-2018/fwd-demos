5070f8f74e8c4aee2033a2ff27e1f94c64210b36
Karl Stolley <karl.stolley@gmail.com>
2018-10-29 15:42:33 -0500

    Add IIFE and quick browser feature test

---
+++ week-11/site.js
@@ -0,0 +1,14 @@
+// Use IIFE (Immediately Invoked Function Expression)
+// See https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+(function(){
+  // First, make sure we're dealing with an A-grade browser
+  if (typeof(document.querySelector)==="undefined") {
+    // if document.querySelector is undefined, then just
+    // return and get out
+    return;
+  }
+
+  // Now we can get down to business and write modern
+  // unobtrusive JavaScript...
+
+})();
