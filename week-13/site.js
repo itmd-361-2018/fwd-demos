233f8251c8de03d06d073a538cfb87080b1db3cd
Karl Stolley <karl.stolley@gmail.com>
2018-11-12 16:29:39 -0600

    Handle expanding nav

--- week-13/site.js
+++ week-13/site.js
@@ -16,8 +16,19 @@ var nav_heading = document.createElement('h2');
 // Build the inner anchor/link element
 var menu_link = document.createElement('a');
 menu_link.textContent = 'Menu';
+menu_link.setAttribute('id','menu-button');
+menu_link.setAttribute('href','#null');
+menu_link.setAttribute('tabindex', '1');
 // Append the menu_link to the heading element
 nav_heading.append(menu_link);
 
 // Finally, append the nav_heading to #navigation
 navigation.insertBefore(nav_heading,nav_list);
+
+// Listen for clicks on #menu-button and toggle the
+// visible class
+var menu_button = document.querySelector('#menu-button');
+menu_button.addEventListener('click', function(e) {
+  nav_list.classList.toggle('visible');
+  e.preventDefault();
+})
